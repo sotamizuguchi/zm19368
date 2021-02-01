532076bbf3813aab531196c55a31acb4c0caf929
sotamizuguchi <zm19368@josai.ac.jp>
2021-02-01 13:38:37 +0000

    message

--- ex6-10.js
+++ ex6-10.js
@@ -1,22 +1,22 @@
-function hyoji() { 
-//表示する文字
-varstr= "一文字ずつ表示します。";
-//テキストボックスの文字数
-varcnt= document.timer.moji.value.length; 
-//文字が全部表示されているか確認
+function hyoji(){
+  //表示する文字
+  var str = "C-moon";
+  //テキストボックスの文字の数
+  var cnt = document.timer.moji.value.length;
+  //文字が全部表示されているかを確認
+  
+  if(cnt < 6){
+    //現在より1文字多く切り出して表示
+    document.timer.moji.value=str.substr(0,cnt+1);}
+    
+  else{
+    //全て表示されたら、空文字に戻す。
+    document.timer.moji.value= "";}}
 
-if ( cnt< 11 ) { 
-//現在より1文字多く切り出して表示
-document.timer.moji.value= str.substr(0,cnt+1); } 
-else { 
-//全て表示されたら、空文字に戻す
-document.timer.moji.value= ""; } } 
+function startfnc(){
+  //関数hyoji()を1000ミリ間隔で呼び出す
+  setInterval("hyoji()",1000);}
 
-function startfnc() {
-//関数hyoji()を1000ミリ秒間隔で呼び出す
-setInterval("hyoji()",1000); } 
-
-function mes() { 
-alert("3秒経ちました！"); 
+function mes(){
+  alert("3秒経過！");
 }
-
